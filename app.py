e68752351cf858c2a4b0da93c0abdeb7ad5643ab
TM-MKD <thomas.mitchell@mkdons.com>
2026-03-03 08:54:45 +0000

    Increase left padding in header table style

--- app.py
+++ app.py
@@ -325,7 +325,7 @@ def generate_pdf():
     header_table.setStyle(TableStyle([
         ("VALIGN", (0,0), (-1,-1), "MIDDLE"),
         ("BACKGROUND", (0,0), (-1,-1), MK_LIGHT_GREY),
-        ("LEFTPADDING", (0,0), (-1,-1), 10),
+        ("LEFTPADDING", (0,0), (-1,-1), 30),
         ("RIGHTPADDING", (0,0), (-1,-1), 10),
         ("TOPPADDING", (0,0), (-1,-1), 10),
         ("BOTTOMPADDING", (0,0), (-1,-1), 10),
