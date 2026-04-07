81319befe8fbffc0adf032c95639133d812e3185
TM-MKD <thomas.mitchell@mkdons.com>
2026-04-07 13:22:16 +0100

    Update header text for Individual Coach View

--- pages/1_Individual_Coach_View.py
+++ pages/1_Individual_Coach_View.py
@@ -27,7 +27,7 @@ with col1:
 
 with col2:
     st.markdown(
-        "<h1 style='margin-bottom:0;'>MK Dons – CEF Sandbox</h1>",
+        "<h1 style='margin-bottom:0;'>Coach Evaluation Framework - Individual Coach View</h1>",
         unsafe_allow_html=True
     )
 
