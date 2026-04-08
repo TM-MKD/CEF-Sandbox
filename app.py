bbd12531a855381ee1c9a088a85cb9527c5bf8dc
TM-MKD <thomas.mitchell@mkdons.com>
2026-04-08 16:05:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
+from auth import enforce_mkdons_sso
+
 st.set_page_config(
     page_title="Home",       
     layout="wide",
     initial_sidebar_state="collapsed"
 )
+
+enforce_mkdons_sso()
 
 st.markdown(
     """
